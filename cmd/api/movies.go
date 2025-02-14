932d23292d3ced32089cb40576b9b867cc97ed6c
Azathoth-X <ranaprikshit25@gmail.com>
2025-02-14 16:57:41 +0530

    chap2: add movie handlers and routing; implement ID parameter parsing

---
+++ cmd/api/movies.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+
+	id, err := app.readIdParam(r)
+
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "%d", id)
+
+}

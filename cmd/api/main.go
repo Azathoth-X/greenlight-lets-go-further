aaa6a9681e6a2a9c7f775e4d82b1143cd13f07a8
Azathoth-X <ranaprikshit25@gmail.com>
2025-02-19 21:50:22 +0530

    5.3:add database connection pool configuration options

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -20,7 +20,10 @@ type config struct {
 	port int
 	env  string
 	db   struct {
-		dsn string
+		dsn         string
+		maxIdleConn int
+		maxOpenConn int
+		maxIdleTime time.Duration
 	}
 }
 
@@ -36,6 +39,10 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "developement", "Environment (developement|staging|production)")
 	flag.StringVar(&cfg.db.dsn, "dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+
+	flag.IntVar(&cfg.db.maxOpenConn, "db-max-open-conn", 25, "PostgreSQL max db open connections")
+	flag.IntVar(&cfg.db.maxIdleConn, "db-max-idle-conn", 25, "PostgreSQL max db idle connections")
+	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
@@ -79,6 +86,10 @@ func openDB(cfg config) (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	db.SetMaxIdleConns(cfg.db.maxIdleConn)
+	db.SetMaxOpenConns(cfg.db.maxOpenConn)
+	db.SetConnMaxIdleTime(cfg.db.maxIdleTime)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()

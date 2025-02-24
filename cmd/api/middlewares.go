7189533ae49f34d67f75c699bf0f1ef0f94783b3
Azathoth-X <ranaprikshit25@gmail.com>
2025-02-24 12:42:36 +0530

    10.1: implement rate limiting middleware and add response for rate limit exceeded

--- cmd/api/middlewares.go
+++ cmd/api/middlewares.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"golang.org/x/time/rate"
 )
 
 func (app *application) recoverPanicMiddleware(next http.HandlerFunc) http.HandlerFunc {
@@ -19,5 +21,20 @@ func (app *application) recoverPanicMiddleware(next http.HandlerFunc) http.Handl
 
 		next.ServeHTTP(w, r)
 
+	})
+}
+
+func (app *application) rateLimit(next http.HandlerFunc) http.HandlerFunc {
+
+	limiter := rate.NewLimiter(2, 4)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		if !limiter.Allow() {
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
 	})
 }

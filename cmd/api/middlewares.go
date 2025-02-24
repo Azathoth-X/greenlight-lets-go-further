f85ab27215527b9fe673f0b08069c93ee2586a91
Azathoth-X <ranaprikshit25@gmail.com>
2025-02-24 15:04:49 +0530

    10.2: implement client-specific rate limiting with dynamic cleanup of inactive clients

--- cmd/api/middlewares.go
+++ cmd/api/middlewares.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
@@ -26,14 +29,55 @@ func (app *application) recoverPanicMiddleware(next http.HandlerFunc) http.Handl
 
 func (app *application) rateLimit(next http.HandlerFunc) http.HandlerFunc {
 
-	limiter := rate.NewLimiter(2, 4)
+	type client struct {
+		lastSeen    time.Time
+		rateLimiter *rate.Limiter
+	}
+
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			mu.Lock()
+
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		if !limiter.Allow() {
+		ip, _, err := net.SplitHostPort(r.Host)
+
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+
+		mu.Lock()
+
+		if _, found := clients[ip]; !found {
+			clients[ip] = &client{rateLimiter: rate.NewLimiter(2, 4)}
+		}
+
+		clients[ip].lastSeen = time.Now()
+
+		if !clients[ip].rateLimiter.Allow() {
+			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
+
+		mu.Unlock()
 
 		next.ServeHTTP(w, r)
 	})

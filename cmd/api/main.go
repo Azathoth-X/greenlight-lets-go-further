8054e666b8499bd09f773015ab52d562746cf09c
Azathoth-X <ranaprikshit25@gmail.com>
2025-02-26 16:57:38 +0530

    11.3: graceful shutdown included to catch system interrupts

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,10 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
 	"greenlight/internal/data"
 	"log/slog"
-	"net/http"
 	"os"
 	"time"
 
@@ -77,22 +75,12 @@ func main() {
 		models: model,
 	}
 
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	err = app.serve()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
 	}
 
-	logger.Info("Server starting", "addr", cfg.port, "env", cfg.env)
-
-	err = srv.ListenAndServe()
-
-	logger.Error(err.Error())
-
-	os.Exit(1)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
---
+++ cmd/api/server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func (app *application) serve() error {
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
+	}
+
+	app.logger.Info("Server starting", "addr", srv.Addr, "env", app.config.env)
+
+	errShutdown := make(chan error)
+	go func() {
+
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+
+		s := <-quit
+
+		app.logger.Info("server shutting down ", "signal", s.String())
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		errShutdown <- srv.Shutdown(ctx)
+	}()
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		if !errors.Is(err, http.ErrServerClosed) {
+			return err
+		}
+	}
+
+	err = <-errShutdown
+	if err != nil {
+		return err
+	}
+
+	app.logger.Info("stopped server", "addr", srv.Addr)
+
+	return nil
+}
